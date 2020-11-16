6019d2bb901002d6d536457a4a39a16e06879e75
st1310 <stetsenkoandrew@gmail.com>
2020-11-16 21:34:33 +0100

    Refactor

---
+++ masgk/PrimHelper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masgk
+{
+    public static class PrimHelper
+    {
+        public static void Cube(Rasterizer rast, VertexProcessor proc)
+        {
+            rast.Triangle(
+                proc.Tr(new Float3(-1f, -1f, 1f)),
+                proc.Tr(new Float3(1f, 1f, 1f)),
+                proc.Tr(new Float3(1f, -1f, 1f)),
+                (Float3)Color.Red, (Float3)Color.Red, (Float3)Color.Red);
+            rast.Triangle(
+                proc.Tr(new Float3(-1f, -1f, 1f)),
+                proc.Tr(new Float3(-1f, 1f, 1f)),
+                proc.Tr(new Float3(1f, 1f, 1f)),
+                (Float3)Color.Red, (Float3)Color.Red, (Float3)Color.Red);
+            rast.Triangle(
+                proc.Tr(new Float3(1f, -1f, 1f)),
+                proc.Tr(new Float3(1f, 1f, -1f)),
+                proc.Tr(new Float3(1f, -1f, -1f)),
+                (Float3)Color.Blue, (Float3)Color.Blue, (Float3)Color.Blue);
+            rast.Triangle(
+                proc.Tr(new Float3(1f, -1f, 1f)),
+                proc.Tr(new Float3(1f, 1f, 1f)),
+                proc.Tr(new Float3(1f, 1f, -1f)),
+                (Float3)Color.Blue, (Float3)Color.Blue, (Float3)Color.Blue);
+            rast.Triangle(
+                proc.Tr(new Float3(1f, -1f, -1f)),
+                proc.Tr(new Float3(-1f, 1f, -1f)),
+                proc.Tr(new Float3(-1f, -1f, -1f)),
+                (Float3)Color.Red, (Float3)Color.Red, (Float3)Color.Red);
+            rast.Triangle(
+                proc.Tr(new Float3(1f, -1f, -1f)),
+                proc.Tr(new Float3(1f, 1f, -1f)),
+                proc.Tr(new Float3(-1f, 1f, -1f)),
+                (Float3)Color.Red, (Float3)Color.Red, (Float3)Color.Red);
+            rast.Triangle(
+                proc.Tr(new Float3(-1f, -1f, -1f)),
+                proc.Tr(new Float3(-1f, 1f, 1f)),
+                proc.Tr(new Float3(-1f, -1f, 1f)),
+                (Float3)Color.Blue, (Float3)Color.Blue, (Float3)Color.Blue);
+            rast.Triangle(
+                proc.Tr(new Float3(-1f, -1f, -1f)),
+                proc.Tr(new Float3(-1f, 1f, -1f)),
+                proc.Tr(new Float3(-1f, 1f, 1f)),
+                (Float3)Color.Blue, (Float3)Color.Blue, (Float3)Color.Blue);
+            rast.Triangle(
+                proc.Tr(new Float3(-1f, -1f, 1f)),
+                proc.Tr(new Float3(1f, -1f, -1f)),
+                proc.Tr(new Float3(-1f, -1f, -1f)),
+                (Float3)Color.Green, (Float3)Color.Green, (Float3)Color.Green);
+            rast.Triangle(
+                proc.Tr(new Float3(-1f, -1f, 1f)),
+                proc.Tr(new Float3(1f, -1f, 1f)),
+                proc.Tr(new Float3(1f, -1f, -1f)),
+                (Float3)Color.Green, (Float3)Color.Green, (Float3)Color.Green);
+            rast.Triangle(
+                proc.Tr(new Float3(1f, 1f, 1f)),
+                proc.Tr(new Float3(-1f, 1f, -1f)),
+                proc.Tr(new Float3(1f, 1f, -1f)),
+                (Float3)Color.Green, (Float3)Color.Green, (Float3)Color.Green);
+            rast.Triangle(
+                proc.Tr(new Float3(1f, 1f, 1f)),
+                proc.Tr(new Float3(-1f, 1f, 1f)),
+                proc.Tr(new Float3(-1f, 1f, -1f)),
+                (Float3)Color.Green, (Float3)Color.Green, (Float3)Color.Green);
+        }
+
+        public static void TestTriangles(Rasterizer rast, VertexProcessor proc)
+        {
+            rast.Triangle(
+                proc.Tr(new Float3(-0.5f, -0.5f, 0f)),
+                proc.Tr(new Float3(0f, 0.5f, 0f)),
+                proc.Tr(new Float3(0.5f, -0.5f, 0f)),
+                (Float3)Color.Red, (Float3)Color.Blue, (Float3)Color.Green);
+            rast.Triangle(
+                proc.Tr(new Float3(-1.1f, 0.5f, 0.9f)),
+                proc.Tr(new Float3(0.6f, 0f, -0.1f)),
+                proc.Tr(new Float3(-0.9f, -1.1f, 0.5f)),
+                (Float3)Color.Red, (Float3)Color.Blue, (Float3)Color.Green);
+            rast.Triangle(
+                proc.Tr(new Float3(0f, 0.5f, 0f)),
+                proc.Tr(new Float3(1.3f, 0.5f, 0f)),
+                proc.Tr(new Float3(0.5f, -0.5f, 0f)),
+                (Float3)Color.Red, (Float3)Color.Blue, (Float3)Color.Green);
+        }
+    }
+}
--- masgk/Program.cs
+++ masgk/Program.cs
@@ -18,26 +18,12 @@ namespace masgk
             Rasterizer rast = new Rasterizer(buff, depth);
 
             VertexProcessor proc = new VertexProcessor();
-            proc.MultByTrans(new Float3(-1f, 0f, 0f));
-            proc.MultByRot(45f, new Float3(0f, 0f, 1f));
+            proc.MultByTrans(new Float3(0f, 0f, 0f));
+            proc.MultByRot(45f, new Float3(1f, 0f, 0f));
             proc.MultByScale(new Float3(0.5f, 0.5f, 0.5f));
             proc.Lt();
 
-            rast.Triangle(
-                proc.Tr(new Float3(-0.5f, -0.5f, 0f)),
-                proc.Tr(new Float3(0f, 0.5f, 0f)),
-                proc.Tr(new Float3(0.5f, -0.5f, 0f)),
-                (Float3)Color.Red, (Float3)Color.Blue, (Float3)Color.Green);
-            rast.Triangle(
-                proc.Tr(new Float3(-1.1f, 0.5f, 0.9f)),
-                proc.Tr(new Float3(0.6f, 0f, -0.1f)),
-                proc.Tr(new Float3(-0.9f, -1.1f, 0.5f)),
-                (Float3)Color.Red, (Float3)Color.Blue, (Float3)Color.Green);
-            rast.Triangle(
-                proc.Tr(new Float3(0f, 0.5f, 0f)),
-                proc.Tr(new Float3(1.3f, 0.5f, 0f)),
-                proc.Tr(new Float3(0.5f, -0.5f, 0f)),
-                (Float3)Color.Red, (Float3)Color.Blue, (Float3)Color.Green);
+            PrimHelper.Cube(rast, proc);
 
             buff.Save("buffer");
             depth.Save("depth");
