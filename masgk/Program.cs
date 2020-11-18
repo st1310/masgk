8ff7913cbf57eddc1787b1d156e73f0405f07866
st1310 <stetsenkoandrew@gmail.com>
2020-11-18 18:13:58 +0100

    Depth fixes

--- masgk/Program.cs
+++ masgk/Program.cs
@@ -13,17 +13,25 @@ namespace masgk
             timer.Start();
 #endif
             using Buffer buff = new Buffer(300, 300, Color.Aqua);
-            using DepthBuffer depth = new DepthBuffer(300, 300, -1.0f);
+            using DepthBuffer depth = new DepthBuffer(300, 300, 1.0f);
 
             Rasterizer rast = new Rasterizer(buff, depth);
 
             VertexProcessor proc = new VertexProcessor();
-            proc.MultByTrans(new Float3(0f, 0f, 0f));
-            proc.MultByRot(45f, new Float3(1f, 0f, 0f));
             proc.MultByScale(new Float3(0.5f, 0.5f, 0.5f));
+            proc.MultByRot(45f, new Float3(1f, 0f, 0f));
+            proc.MultByTrans(new Float3(-0.5f, 0f, -5.0f));
             proc.Lt();
 
             PrimHelper.Cube(rast, proc);
+
+            VertexProcessor proc1 = new VertexProcessor();
+            proc1.MultByScale(new Float3(0.5f, 0.5f, 0.5f));
+            proc1.MultByRot(0f, new Float3(0f, 1f, 0f));
+            proc1.MultByTrans(new Float3(1.0f, 0f, -5.0f));
+            proc1.Lt();
+
+            PrimHelper.Cube(rast, proc1);
 
             buff.Save("buffer");
             depth.Save("depth");
